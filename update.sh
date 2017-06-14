8a9bb420d4aa22261ca5e4d99eb7fd0078960bfd
Aaron Votre <avotre422@gmail.com>
2017-06-14 11:16:48 -0400

    Add global npm package info to update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 codeSettings=~/dotfiles/vs-settings.json
+globalNpm=~/dotfiles/global-npm.txt
 
 echo "Brew dump"
 brew bundle dump -f
@@ -8,3 +9,5 @@ brew bundle dump -f
 echo "VS Code settings - $codeSettings"
 cp ~/Library/Application\ Support/Code/User/settings.json $codeSettings
 
+echo "Global Npm Packages - $globalNpm"
+npm list -g --depth=0 > $globalNpm
