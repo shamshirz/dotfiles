b29864b250465dd0a915a78887e23ca2cd22fa98
Aaron Votre <aaron@Aarons-MacBook-Pro.local>
2017-06-19 20:47:42 -0400

    Trim dotfiles, remove java stuff

--- env.sh
+++ env.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
+# I'm expecting a $dots var to be something like ~/git/dotfiles or something
+
 # Owner
 export USER_NAME="aaron"
+export wk_dir="~/Git"
+export PATH=$PATH:/usr/local/bin
 
 # FileSearch
 #function f() { find . -iname "*$1*" ${@:2} }
@@ -12,15 +16,15 @@ function mkcd() { mkdir -p "$@" && cd "$_"; }
 
 # Aliases
 
-alias zshconfig="vim ~/.zshrc"
-alias envconfig="vim ~/env.sh"
+alias zshconfig="vim $dots/zshrc_dot_me"
+alias envconfig="vim $dots/env.sh"
+alias upDot="sh $dots/update.sh"
 
 alias start-pg='pg_ctl -l $PGDATA/server.log start'
 alias stop-pg='pg_ctl stop -m fast'
 alias restart-pg='pg_ctl reload'
 alias pgStart='brew services restart postgresql'
 
-alias config='cd ~/Documents/Git/dynamoConfig'
 alias aws='cd ~/.aws/'
 alias pgres='cd /usr/local/var/postgres'
 alias ls="ls -G"
@@ -28,22 +32,9 @@ alias log="git log --pretty=oneline --abbrev-commit"
 alias gs="git status"
 alias ru="git remote update"
 alias gc="git commit"
-alias trump="cd ~/Documents/Git/trump-money-maker"
-alias proj="cd ~/Documents/Git"
-alias upDot="sh ~/dotfiles/update.sh"
 
-# Gradle
-export GRADLE_HOME="/Users/aaron/Library/Gradle/gradle-2.12"
-export PATH=$PATH:$GRADLE_HOME/bin
-
-# Spring
-export SPRING_HOME=/Users/aaron/Library/Spring/spring-1.3.3.RELEASE
-export PATH=$PATH:$SPRING_HOME/bin
-
-# local bin (for Visual Studio)
-export PATH=$PATH:/usr/local/bin
 
 echo "Welcome back"
-cat ~/dotfiles/aaron.txt
+cat $dots/aaron.txt
 echo $(date)
 
