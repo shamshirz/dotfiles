fd72ce86506905f9b79edbf885f763373425bf07
Aaron Votre <avotre422@gmail.com>
2017-10-09 16:04:19 -0400

    Improve Docker script & drop excess deps

--- install/docker.sh
+++ install/docker.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-echo "Getting Docker CE"
-curl "https://download.docker.com/mac/stable/Docker.dmg" -o ~/Downloads/Docker.dmg
+if hash docker 2>/dev/null; then
+  echo "🐋 Docker already Installed. Continuing…"
+else
+  echo "🐋 Getting Docker CE"
+  curl "https://download.docker.com/mac/stable/Docker.dmg" -o ~/Downloads/Docker.dmg
 
-hdiutil attach ~/Downloads/Docker.dmg
+  hdiutil attach ~/Downloads/Docker.dmg
 
-cp -ir /Volumes/Docker/Docker.app /Applications
+  cp -ir /Volumes/Docker/Docker.app /Applications
 
-sudo hdiutil unmount /Volumes/Docker
+  sudo hdiutil unmount /Volumes/Docker
 
-echo "Docker successfully installed"
+  echo "🐋 Docker successfully installed"
+fi
